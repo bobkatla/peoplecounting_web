bb3822145405c31472e73637bc7815e8388051f9
bobkatla <bertram.99.works@gmail.com>
2020-10-09 19:35:02 +1100

    worked, can update every 5s and looking fine

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,15 +12,39 @@ class App extends React.Component {
         super();
         this.state = {
             buildings: [],
-            searchField: ''
+            searchField: '',
+            sum: ''
         }
     }
 
+     /*
+        declare a member variable to hold the interval ID
+        that we can reference later.
+      */
+    intervalID;
+
     componentDidMount() {
+        this.getData();
+        // update value every 5s
+        this.intervalID = setInterval(this.getData.bind(this), 5000);
+    }
+
+    getData = () => {
         fetch(`${api}/getall`)
             .then(response => response.json())
-            .then(buildings => this.setState({buildings}))  
-    }
+            .then(buildings => this.setState({buildings}));
+        fetch(`${api}/getsum`)
+            .then(response => response.json())
+            .then(sum => this.setState({sum}));
+      }
+
+    componentWillUnmount() {
+        /*
+          stop getData() from continuing to run even
+          after unmounting this component
+        */
+        clearInterval(this.intervalID);
+      }
 
     onSearchChange = (event) => {
         this.setState({searchField: event.target.value});
@@ -28,16 +52,17 @@ class App extends React.Component {
 
     render() {
         // using destruct to clean the code
-        const {buildings, searchField} = this.state;
+        const {buildings, searchField, sum} = this.state;
         const filteredBuildings = buildings.filter(building => {
             return building.name.substring(8).toLowerCase().includes(searchField.toLowerCase());
         })
-        
+
         return !buildings.length
         ? <h1 className='tc'>Loading</h1>
         : (
             <div className='tc'>
-                <h1 className='f1'>BUILDINGs</h1>
+                <h1 className='f1'>Counting people project</h1>
+                <h3 className='f2'><i>{`total: ${sum} people inside buildings`}</i></h3>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
                     <CardList buildings={filteredBuildings}/>
